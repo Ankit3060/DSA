5c658a01b8723bde929e1dfd3c4cc76a564a3e94
Ankit3060 <ankit330660@gmail.com>
2024-04-03 18:03:03 +0530

    Fix typo in groupAnagramMethod1()

--- String/groupAnagram.cpp
+++ String/groupAnagram.cpp
@@ -25,7 +25,7 @@ vector<vector<string>> groupAnagramMethod1(vector<string>& strs){
 std::array<int,256> hash(string s){
     std::array<int, 256>hash={0};
     for(int i=0;i<s.size();i++){
-        hash[s[i]-'a']++;
+        hash[s[i]-'a']++; //hash[s[i]]++;
     }
     return hash;
 }
