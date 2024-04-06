f344e4cbf0979f9e3f5627d57eafcef448b21d56
Ankit3060 <ankit330660@gmail.com>
2024-04-06 18:21:35 +0530

    Address of data type

---
+++ Pointers/address.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=5;
+    int b=5;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<&a<<endl;
+    cout<<&b<<endl;
+    return 0;
+}
