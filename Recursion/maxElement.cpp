75c95fa5fef617853e9fc14d2ec62f83764348f3
Ankit3060 <ankit330660@gmail.com>
2024-04-11 19:44:05 +0530

    minimum element in the array using recursion

--- Recursion/maxElement.cpp
+++ Recursion/maxElement.cpp
@@ -10,12 +10,22 @@ void maxElement(int arr[], int n, int& maxi, int i){
     maxElement(arr,n,maxi,i+1);
 }
 
+void minElement(int arr[], int n, int& mini, int i){
+    if(i>=n)
+    return;
+    mini = min(mini,arr[i]);
+    minElement(arr,n,mini,i+1);
+}
+
 int main(){
     int arr[5]= {1,2,3,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     int maxi = INT_MIN;
+    int mini = INT_MAX;
     int i=0;
     maxElement(arr,n,maxi,i);
     cout<<"Maximum element in the array is : "<<maxi;
+    minElement(arr,n,mini,i);
+    cout<<"\nMinimum element in the array is : "<<mini;
     return 0;
 }
