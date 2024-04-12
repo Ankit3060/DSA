2fc029f57a57396b2e1bf993471761145818900b
Ankit3060 <ankit330660@gmail.com>
2024-04-12 11:53:13 +0530

    count a character in a string using recursion

--- Recursion/checkstring.cpp
+++ Recursion/checkstring.cpp
@@ -37,6 +37,16 @@ void checkInedxAll(string& str, int& n, int i, char& key, vector<int>& ans){
     return checkInedxAll(str,n,i+1,key,ans);
 }
 
+void checkIndexCount(string& str, int& n, int i, char& key, int& count){
+    if(i>=n)
+    return;
+
+    if(str[i]==key){
+        count++;
+    }
+    return checkIndexCount(str,n,i+1,key,count);
+}
+
 int main(){
     string str = "ankit kumar";
     int n= str.length();
@@ -54,5 +64,9 @@ int main(){
     for(auto i: ans3){
         cout<<i<<" ";
     }
+
+    int count=0;
+    checkIndexCount(str,n,i,key,count);
+    cout<<"\nCount of "<<key<<" is : "<<count;
     return 0;
 }
