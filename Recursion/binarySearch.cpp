ad755ab9243827024054842bdbaa6d7f015d7186
Ankit3060 <ankit330660@gmail.com>
2024-04-17 19:45:36 +0530

    Binary search using recursion problem solved

---
+++ Recursion/binarySearch.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BS(vector<int>&arr, int s, int e,int key){
+    //Base case
+    if(s>e){
+        return -1;
+    }
+
+    //Finding the mid values
+    int mid = s+(e-s)/2;
+    if(arr[mid]==key){
+        return mid;
+    }
+    // if(arr[mid]>key){
+    //     return BS(arr,s,mid-1,key);
+    // }
+    // else{
+    //     return BS(arr,mid+1,e,key);
+    // }
+
+    //using ternary operator
+    return arr[mid]>key ? BS(arr,s,mid-1,key) : BS(arr,mid+1,e,key);
+}
+
+int main(){
+    vector<int>arr = {1,2,3,4,5,6,7,8,9,10};
+    int n = arr.size();
+    int key = 5;
+    int s=0;
+    int e=n-1;
+    int ans = BS(arr,s,e,key);
+    cout<<"Index of "<<key<<" is : "<<ans<<endl;
+    return 0;
+}
