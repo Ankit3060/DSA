dfc7de75418970e0a263ff7ab766bcdd14722e08
Ankit3060 <ankit330660@gmail.com>
2024-06-20 00:24:58 +0530

    Reorganize string practised

---
+++ Practise/reorganize.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reorganizeString(string s){
+    int hash[26]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]-'a']++;
+    }
+
+    char most_freq_char;
+    int max_freq = INT_MIN;
+    for(int i=0;i<26;i++){
+        if(hash[i]>max_freq){
+            max_freq = hash[i];
+            most_freq_char = i + 'a';
+        }
+    }
+
+    int index=0;
+    while(max_freq>0 && index<s.size()){
+        s[index] = most_freq_char;
+        max_freq--;
+        index += 2;
+    }
+    if(max_freq != 0){
+        return "";
+    }
+
+    hash[most_freq_char - 'a'] =0;
+    for(int i=0;i<26;i++){
+        while(hash[i]>0){
+            index = index >= s.size() ? 1 : index;
+            s[index] = i + 'a';
+            hash[i]--;
+            index += 2;
+        }
+    }
+    return s;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    string ans = reorganizeString(s);
+    cout << ans;
+    return 0;
+}
