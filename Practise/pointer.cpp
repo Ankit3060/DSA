f395582a6939eee6e8d6b1f5b607acad7f6f23a3
Ankit3060 <ankit330660@gmail.com>
2024-06-21 00:46:25 +0530

    pointer practised done

---
+++ Practise/pointer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void updatePtr(int *p){
+    int a = 10;
+    p=&a;
+    *p=(*p) * (*p);
+}
+
+int main()
+{
+    // int a = 12;
+    // int *ptr = &a;
+    // cout<<a<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<&a<<endl;
+    
+    // cout<<"Pass by value"<<endl;
+    // updatePtr(a);
+    // cout<<a<<endl;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<&a<<endl;
+    
+    // int a=12;
+    // int *p=&a;
+    // int **q=&p;
+    // cout<<a<<endl;
+    // cout<<p<<endl;
+    // cout<<q<<endl;
+    // cout<<&a<<endl;
+    // cout<<&p<<endl;
+    // cout<<&q<<endl;
+    // cout<<*p<<endl;
+    // cout<<**q<<endl;
+    // cout<<a<<endl;
+    
+    int a=10;
+    updatePtr(&a);
+    cout<<a;
+    
+    
+    
+    return 0;
+}
