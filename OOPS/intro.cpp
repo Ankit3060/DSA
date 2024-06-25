bb76ccc8bb61e330f2e9bbb68f18e4ad9e2ea331
Ankit3060 <ankit330660@gmail.com>
2024-06-26 00:10:47 +0530

    Creating of class and object basic done

---
+++ OOPS/intro.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+// #include"Hero.cpp"
+using namespace std;
+
+class Hero{
+
+    //Properties
+    int health;
+    char level;
+
+};
+
+int main(){
+    Hero h1;
+    cout<<"Size is : "<<sizeof(h1)<<endl;
+    return 0;
+}
