ef3e570198fa63b996cfed01ecc006b6b3624040
Ankit3060 <ankit330660@gmail.com>
2024-08-11 01:26:21 +0530

    First non repeating element done

---
+++ Queue/firstNonRepeating.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string FirstNonRepeating(string A){
+    unordered_map<char,int>count;
+    string ans = "";
+    queue<int>q;
+
+    for(int i=0;i<A.length();i++){
+        char ch = A[i];
+
+        //Count
+        count[ch]++;
+
+        //push in queue
+        q.push(ch);
+
+        //Checking repeating or not if yes then pop else push ans
+        while(!q.empty()){
+            if(count[q.front()]>1){
+                q.pop();
+            }else{
+                ans.push_back(q.front());
+                break;
+            }
+        }
+
+        //checking if empty then push # in that place
+        if(q.empty()){
+            ans.push_back('#');
+        }
+    }
+    return ans;
+}
+
+int main(){
+    string input = "aabc";
+    string result = FirstNonRepeating(input);
+
+    cout << "First non-repeating characters sequence: " << result << endl;
+    return 0;
+}
