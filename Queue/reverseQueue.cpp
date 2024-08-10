ec93ea03cd1269c3670c8928fb1792e158a18da4
Ankit3060 <ankit330660@gmail.com>
2024-08-11 01:04:07 +0530

    Reversal of queue is done

---
+++ Queue/reverseQueue.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+queue<int> reverse(queue<int>q){
+    stack<int>s;
+
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
+
+int main(){
+    queue<int> q;
+
+    // Push elements into the queue
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+
+    //Queue before reversal
+    queue<int> temp_q = q; // Use a temporary queue to preserve the original queue
+    cout<<"Original Queue: ";
+    while (!temp_q.empty()) {
+        cout << temp_q.front() << " ";
+        temp_q.pop();
+    }
+    cout << endl;
+
+    // Reverse the queue
+    q = reverse(q);
+
+    // Print the reversed queue
+    cout << "Reversed Queue: ";
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
