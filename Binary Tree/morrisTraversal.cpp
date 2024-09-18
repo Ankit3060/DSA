fc2caaba5f054b1adc23baa27bee1ac5ea35bd1f
Ankit3060 <ankit330660@gmail.com>
2024-09-19 01:19:17 +0530

    Morris Traversal problem done using inorder

---
+++ Binary Tree/morrisTraversal.cpp
@@ -0,0 +1,120 @@
+// This is Morris Traversal problem with inorder property
+//        1
+//       / \ 
+//      3   5
+//     /\   /\
+//    7 11 17 
+// Morris Traversal : 7 3 11 1 17 5 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrderBuldTree(Node* &root) {
+    queue<Node*> q;
+    int data;
+    cout << "Enter the data : " << endl;
+    cin >> data;
+    root = new Node(data);
+    q.push(root);
+
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        cout << "Enter the left data : " << endl;
+        int leftData;
+        cin >> leftData;
+        if (leftData != -1) {
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout << "Enter the right data : " << endl;
+        int rightData;
+        cin >> rightData;
+        if (rightData != -1) {
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+void morrisTraversal(Node* root){
+    Node* current = root;
+    while(current != NULL){
+        if(current->left == NULL){
+            cout<<current->data<<" ";
+            current = current->right;
+        }
+
+        else{
+            Node* precedor = current->left;
+            while(precedor->right != NULL && precedor->right != current){
+                precedor = precedor->right;
+            }
+
+            if(precedor->right==NULL){
+                precedor->right = current;
+                current = current->left;
+            }else{
+                precedor->right = NULL;
+                cout<<current->data<<" ";
+                current = current->right;
+            }
+        }
+    }
+}
+
+
+void levelOrderTraversal(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+
+        if (temp == NULL) {
+            cout << endl;
+            if (!q.empty()) {
+                q.push(NULL);
+            }
+        } else {
+            cout << temp->data << " ";
+            if (temp->left) {
+                q.push(temp->left);
+            }
+            if (temp->right) {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+int main() {
+    Node* root = NULL;
+
+    levelOrderBuldTree(root);
+
+    cout << "Level Order Traversal of the tree:" << endl;
+    levelOrderTraversal(root);
+
+    cout << "Morris Traversal:" << endl;
+    morrisTraversal(root);  // calling Morris traversal for the right view
+
+    return 0;
+}
